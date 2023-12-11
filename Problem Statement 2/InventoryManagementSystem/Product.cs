88a29c209ecc3f57e5ec14dee18054fb03ae2a7c
Mukul Palol <mukul.palol@intimetec.com>
2023-12-11 12:07:11 +0530

    renamed code of problem statement 2

---
+++ Problem Statement 2/InventoryManagementSystem/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace InventoryManagementSystem
+{
+    public class Product
+    {        
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }     
+    }
+}
---
+++ Problem Statement 2/InventoryManagementSystem/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace InventoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManager inventoryManager = new InventoryManager();
+
+            while (true)
+            {
+                Console.WriteLine("\nInventory Management System");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Display Inventory");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter product name: ");
+                        string productName = Console.ReadLine();
+                        Console.Write("Enter product price: ");
+                        double productPrice = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter product quantity: ");
+                        int productQuantity = Convert.ToInt32(Console.ReadLine());
+
+                        Product product = new Product(productName, productPrice, productQuantity);
+                        inventoryManager.AddProduct(product);
+
+                        Console.WriteLine("Product added successfully!");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nCurrent Inventory:");
+                        List<Product> currentInventory = inventoryManager.GetInventory();
+                        foreach (Product currentProduct in currentInventory)
+                        {
+                            Console.WriteLine($"Name: {currentProduct.Name}, Price: ${currentProduct.Price}, Quantity: {currentProduct.Quantity}");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+            }
+        }
+    }
+}
