d09e95f5aa3aa002feebd5996eb4ef0bc5c6cb64
Mukul Palol <mukul.palol@intimetec.com>
2023-12-11 11:01:38 +0530

    renamed in problem statement 1

---
+++ Problem Statement 1/BasicUtility/Utility.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicUtility
+{
+    public class Utility
+    {
+        public int IntegerValue { get; set; }
+        public double DoubleValue { get; set; }
+        public string StringValue { get; set; }
+
+        public void calculateSum(int input1, int input2)
+        {
+            int sum = input1 + input2;
+            Console.WriteLine("Sum: " + sum);
+        }
+
+        public void convertToUpperCase(string inputString)
+        {
+            StringValue = inputString.ToUpper();
+            Console.WriteLine("Uppercase String: " + StringValue);
+        }
+    }
+}
